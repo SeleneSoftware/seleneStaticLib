f487ba8175c527ffbfe89adb5ffb11276227f55f
Jason Marshall <jasonmarshall@selenesoftware.us>
2018-11-22 01:00:51 -0500

    if a page does not have global variables, it was throwing an error

--- src/Application.php
+++ src/Application.php
@@ -1,21 +1,5 @@
 <?php
 
-/**
-     Copyright 2017 Selene Software - selenesoftware.us
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
-
 namespace App;
 
 use Aptoma\Twig\Extension\MarkdownEngine\ParsedownEngine;
@@ -33,7 +17,7 @@ class Application
     {
         $this->loader = new \Twig_Loader_Filesystem(__DIR__ . '/../templates');
         $this->twig =new \Twig_Environment($this->loader, [
-            'cache' => __DIR__ . '/../cache',
+            // 'cache' => __DIR__ . '/../cache',
         ]);
 
         $engine = new ParsedownEngine;
@@ -45,11 +29,6 @@ class Application
         foreach ($this->global['global'] as $key => $value) {
             $this->twig->addGlobal($key, $value);
         }
-    }
-
-    public function getTwig()
-    {
-        return $this->twig;
     }
 
     public function run()
@@ -84,7 +63,11 @@ class Application
                             mkdir($dirname, 0755, true);
                         }
                         $f = fopen(__DIR__ . '/../web/' . $pageName . '/' . 'index.html', 'w');
-                        $vars = $this->global[$pageName];
+
+                        $vars = [];
+                        if (isset($this->global[$pageName])) {
+                            $vars = $this->global[$pageName];
+                        }
                         if (file_exists(__DIR__ . '/../content/'.$pageName.'.md')) {
                             $vars['pagecontent'] = file_get_contents(__DIR__ . '/../content/'.$pageName.'.md');
                         }

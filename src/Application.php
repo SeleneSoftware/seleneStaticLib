0b7112bf89b042120785e67f58056076e357fac5
Jason Marshall <jasonmarshall@selenesoftware.us>
2019-06-04 14:50:44 -0400

    added method to add a Twig extension, php-cs-fixer config

--- src/Application.php
+++ src/Application.php
@@ -5,74 +5,81 @@ namespace Selene\StaticSite;
 use Aptoma\Twig\Extension\MarkdownEngine\ParsedownEngine;
 use Aptoma\Twig\Extension\MarkdownExtension;
 use Symfony\Component\Yaml\Yaml;
+use Twig\Extension\ExtensionInterface;
 
 class Application
 {
     protected $loader;
+
     protected $twig;
+
     protected $config;
+
     protected $global;
+
     protected $dir;
 
     public function __construct()
     {
         $this->dir = getcwd();
-        $this->loader = new \Twig_Loader_Filesystem($this->dir . '/templates');
-        $this->twig =new \Twig_Environment($this->loader, [
+        $this->loader = new \Twig_Loader_Filesystem($this->dir.'/templates');
+        $this->twig = new \Twig_Environment($this->loader, [
             // 'cache' => __DIR__ . '/../cache',
         ]);
 
-        $engine = new ParsedownEngine;
+        $engine = new ParsedownEngine();
 
         $this->twig->addExtension(new MarkdownExtension($engine));
 
-        $this->config = Yaml::parseFile($this->dir . '/config/config.yml');
-        $this->global = Yaml::parseFile($this->dir . '/config/global.yml');
+        $this->config = Yaml::parseFile($this->dir.'/config/config.yml');
+        $this->global = Yaml::parseFile($this->dir.'/config/global.yml');
         foreach ($this->global['global'] as $key => $value) {
             $this->twig->addGlobal($key, $value);
         }
     }
 
+    public function addExtension(ExtensionInterface $extension)
+    {
+        $this->twig->addExtension($extension);
+    }
+
     public function run()
     {
-
-        $pages = scandir($this->dir . '/templates/pages');
+        $pages = scandir($this->dir.'/templates/pages');
         foreach ($pages as $page) {
-            $p = $this->dir . '/templates/pages/' . $page;
+            $p = $this->dir.'/templates/pages/'.$page;
             if (is_file($p)) {
                 $info = pathinfo($p);
-                if ($info['extension'] === 'twig') {
-                    $template = $this->twig->load('pages/' . $page);
+                if ('twig' === $info['extension']) {
+                    $template = $this->twig->load('pages/'.$page);
 
                     $vars = $this->global['index'];
-                    if ($info['filename'] === 'index.html') {
-                        $dirname = dirname($this->dir . '/web/' . $info['filename']);
-                        if (!is_dir($dirname))
-                        {
+                    if ('index.html' === $info['filename']) {
+                        $dirname = dirname($this->dir.'/web/'.$info['filename']);
+                        if (!is_dir($dirname)) {
                             mkdir($dirname, 0755, true);
                         }
 
-                        $f = fopen($this->dir . '/web/' . $info['filename'], 'w');
+                        $f = fopen($this->dir.'/web/'.$info['filename'], 'w');
 
-                        if (file_exists($this->dir . '/content/index.md')) {
-                            $vars['pagecontent'] = file_get_contents($this->dir . '/content/index.md');
+                        if (file_exists($this->dir.'/content/index.md')) {
+                            $vars['pagecontent'] = file_get_contents($this->dir.'/content/index.md');
                         }
                         $vars['pagename'] = 'index';
                     } else {
                         $pageName = substr($info['filename'], 0, -5);
-                        $dirname = dirname($this->dir . '/web/' . $pageName. '/' . 'index.html');
-                        if (!is_dir($dirname))
-                        {
+                        $dirname = dirname($this->dir.'/web/'.$pageName.'/'.'index.html');
+                        if (!is_dir($dirname)) {
                             mkdir($dirname, 0755, true);
                         }
-                        $f = fopen($this->dir . '/web/' . $pageName . '/' . 'index.html', 'w');
+                        $f = fopen($this->dir.'/web/'.$pageName.'/'.'index.html', 'w');
 
                         $vars = [];
                         if (isset($this->global[$pageName])) {
                             $vars = $this->global[$pageName];
                         }
-                        if (file_exists($this->dir . '/content/'.$pageName.'.md')) {
-                            $vars['pagecontent'] = file_get_contents($this->dir . '/content/'.$pageName.'.md');
+                        if (file_exists($this->dir.'/content/'.$pageName.'.md')) {
+                            $vars['pagecontent'] = file_get_contents($this->dir.'/content/'.$pageName.'.md');
                         }
                         $vars['pagename'] = $pageName;
                     }
@@ -83,5 +90,3 @@ class Application
         }
     }
 }
-
-
